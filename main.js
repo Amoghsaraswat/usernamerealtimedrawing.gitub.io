f5a945c3492323c8472c2e6cb0498a23796c71a4
Amoghsaraswat <75422601+Amoghsaraswat@users.noreply.github.com>
2021-09-04 16:48:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftwristX=0;
+rightwristX=0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -13,10 +18,21 @@ function modelLoaded() {
 
 function draw() {
     background("white");
+    document.getElementById("square").innerHTML="width and height of a square will be "+difference+"px";
+    fill("red");
+    stroke("red");
+    square(noseX,noseY,difference);
 }
 
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+       noseX=results[0].pose.nose.x;
+               noseY=results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY ="+noseY);
+        leftwristX=results[0].pose.leftWrist.x;
+        rightwristX=results[0].pose.rightWrist.x;
+        difference=floor(leftwristX-rightwristX);
+    
     }
 }
